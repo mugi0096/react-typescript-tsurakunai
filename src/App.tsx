bbd9e16fd2bace166eceecc7969eb8310339355c
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-04 15:48:31 +0900

    ✨ add evenOrOdd function

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,32 @@ import './App.css';
 const App: FC = () => {
   const name = 'Patty';
   const greet = (name: string) => <p>Hello, {name || 'Guest'}!</p>;
+
+  const n = Math.floor(Math.random() * 10);
+  const threshold = 5;
+  const evenOrOdd = (num: number) => (
+    <div>
+      {n > threshold && (
+        <p>
+          {n} は {threshold} よりも大きい値です
+        </p>
+      )}
+      {n > threshold || (
+        <p>
+          {n} は {threshold} 以下の値です
+        </p>
+      )}
+      <p>
+        {n} は {n % 2 === 0 ? '偶数' : '奇数'}です
+      </p>
+    </div>
+  );
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>{greet(name)}</div>
+        {evenOrOdd(n)}
       </header>
     </div>
   );
