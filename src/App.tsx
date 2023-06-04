531d9566376404c3c8a437dc5ca0263753027204
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-04 15:37:34 +0900

    ✨ add greet function

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './assets/react.svg';
 import './App.css';
 
 const App: FC = () => {
+  const name = 'Patty';
+  const greet = (name: string) => <p>Hello, {name || 'Guest'}!</p>;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
+        <div>{greet(name)}</div>
       </header>
     </div>
   );
