15786d22f0286a5c7e6d7fda944664d0a66bc163
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-06 22:25:18 +0900

    🎨 Put props into args

--- src/components/Greet/Greet.tsx
+++ src/components/Greet/Greet.tsx
@@ -1,8 +1,6 @@
 type Props = { name: string; times?: number };
 
-const Greet: React.FC<Props> = (props) => {
-  const { name, times = 1 } = props;
-
+const Greet: React.FC<Props> = ({ name, times = 1 }) => {
   return (
     <>
       {[...Array(times)].map(() => (
