766ce4c668ef5cfbda88d7d11bdb4b33ac04bd2a
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-08 09:30:01 +0900

    🎨 add key to array items

--- src/components/Greet/Greet.tsx
+++ src/components/Greet/Greet.tsx
@@ -3,8 +3,8 @@ type Props = { name: string; times?: number };
 const Greet: React.FC<Props> = ({ name, times = 1 }) => {
   return (
     <>
-      {[...Array(times)].map(() => (
-        <p>Hello, {name}!</p>
+      {[...Array(times).keys()].map((i) => (
+        <p key={i}>Hello, {name}!</p>
       ))}
     </>
   );
