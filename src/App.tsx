e305f16180dac38f4ce1aabafbe5c3ab1eecf37e
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-27 23:14:10 +0900

    ✨ use only character list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,49 @@
 import { type FC } from 'react';
-import logo from './assets/react.svg';
+import { Heading } from '@chakra-ui/react';
 import './App.css';
 
-import Greet from './components/Greet/Greet';
-import Summary from './components/Summary/Summary';
-import TextInput from './components/TextInput/TextInput';
-
-const title = import.meta.env.VITE_APP_TITLE;
-console.dir(import.meta.env);
+import CharacterList from 'components/CharacterList/CharacterList';
+import { type Character } from 'components/CharacterList/CharacterList';
 
 const App: FC = () => {
-  const n = Math.floor(Math.random() * 10);
-  const threshold = 5;
-  const evenOrOdd = (num: number, threshold: number) => (
-    <div>
-      {num > threshold && (
-        <p>
-          {num} は {threshold} よりも大きい値です
-        </p>
-      )}
-      {num > threshold || (
-        <p>
-          {num} は {threshold} 以下の値です
-        </p>
-      )}
-      <p>
-        {num} は {num % 2 === 0 ? '偶数' : '奇数'}です
-      </p>
-    </div>
-  );
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '桜木花道',
+      grade: 1,
+      height: 189.2,
+    },
+    {
+      id: 2,
+      name: '流川 楓',
+      grade: 1,
+      height: 187,
+    },
+    {
+      id: 3,
+      name: '宮城リョータ',
+      grade: 2,
+      height: 168,
+    },
+    {
+      id: 4,
+      name: '三井 寿',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: '赤木剛憲',
+      grade: 3,
+      height: 197,
+    },
+  ];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{title}</h1>
-        <Greet name="Patty" times={1} />
-        {evenOrOdd(n, threshold)}
-      </header>
-      <Summary title="maple Town">
-        <p>
-          Patty Hope-rabbit, along with her family, arrives in Maple Town, a
-          small town inhabited by friendly animals.
-        </p>
-        <p>
-          Soon, the Rabbit Family settles in Maple Town as mail carriers and the
-          bitter, yet sweet friendship of Patty and Bobby begins to blossom.
-        </p>
-      </Summary>
-      <TextInput />
+      <Heading size="lg" as="h1" my={12}>
+        『SLAM DUNK』登場人物
+      </Heading>
+      <CharacterList school="湘北高校" characters={characters} />
     </div>
   );
 };
