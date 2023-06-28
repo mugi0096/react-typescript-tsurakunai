ffd167e93b1f4f3d033a6999bfa8a766da0905e5
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-06-28 23:02:26 +0900

    🎨 make component definition simpler

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -21,33 +21,29 @@ type Props = {
   characters: Character[];
 };
 
-const CharacterList: FC<Props> = (props) => {
-  const { school, characters } = props;
-
-  return (
-    <div>
-      <Heading size="md" as="h2">
-        {school}
-      </Heading>
-      <List my={8}>
-        {characters.map((character) => (
-          <ListItem key={character.id} m={6}>
-            <Flex>
-              <Avatar size="md" bg="teal.500" />
-              <Box textAlign="left" ml={3}>
-                <Text>{character.name}</Text>
-                <Text as="span">{character.grade}年生</Text>
-                <Text as="span" ml={2}>
-                  {character.height != null ? character.height : '???'}
-                  cm
-                </Text>
-              </Box>
-            </Flex>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-};
+const CharacterList: FC<Props> = ({ school, characters }) => (
+  <div>
+    <Heading size="md" as="h2">
+      {school}
+    </Heading>
+    <List my={8}>
+      {characters.map((character) => (
+        <ListItem key={character.id} m={6}>
+          <Flex>
+            <Avatar size="md" bg="teal.500" />
+            <Box textAlign="left" ml={3}>
+              <Text>{character.name}</Text>
+              <Text as="span">{character.grade}年生</Text>
+              <Text as="span" ml={2}>
+                {character.height ?? '???'}
+                cm
+              </Text>
+            </Box>
+          </Flex>
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
 
 export default CharacterList;
