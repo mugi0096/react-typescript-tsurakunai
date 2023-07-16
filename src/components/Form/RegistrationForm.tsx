0c9182441447b26ba6feabcb11fd0f2f64dc949d
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-07-16 17:13:47 +0900

    🎨 use react hook form

--- src/components/Form/RegistrationForm.tsx
+++ src/components/Form/RegistrationForm.tsx
@@ -1,5 +1,4 @@
-import type { FC, ChangeEvent, SyntheticEvent } from 'react';
-import { useState } from 'react';
+import type { FC, SyntheticEvent } from 'react';
 import {
   Box,
   Button,
@@ -9,6 +8,8 @@ import {
   Input,
   Select,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 
 const genderCode = { f: '女性', m: '男性', n: 'それ以外' } as const;
 
@@ -20,71 +21,46 @@ interface FormData {
 }
 
 const RegistrationForm: FC = () => {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    isAgreed: false,
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: {
+      username: '',
+      isAgreed: false,
+    },
   });
-
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault();
-    console.log(formData);
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
   };
-
-  const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name } = event.target;
-    const value =
-      event.target.type === 'checkbox'
-        ? (event.target as HTMLInputElement).checked
-        : event.target.value;
-    setFormData((state) => ({ ...state, [name]: value }));
+  const onReset = (e: SyntheticEvent) => {
+    e.stopPropagation();
+    reset();
   };
 
   return (
     <Box p={5} w="md" borderWidth="1px" borderRadius="lg" boxShadow="base">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormLabel htmlFor="username" mt={2}>
           ユーザー名
         </FormLabel>
-        <Input
-          name="username"
-          size="md"
-          value={formData.username}
-          onChange={handleChange}
-        />
+        <Input size="md" {...register('username')} />
         <FormLabel htmlFor="zipcode" mt={4}>
           郵便番号
         </FormLabel>
-        <Input
-          name="zipcode"
-          size="md"
-          maxLength={7}
-          value={formData.zipcode ?? ''}
-          onChange={handleChange}
-        />
-        <Select
-          name="gender"
-          my={6}
-          placeholder="性別を選択・・・"
-          value={formData.gender}
-          onChange={handleChange}
-        >
+        <Input size="md" maxLength={7} {...register('zipcode')} />
+        <Select my={6} placeholder="性別を選択・・・" {...register('gender')}>
           {Object.entries(genderCode).map(([code, name]) => (
             <option value={code} key={code}>
               {name}
             </option>
           ))}
         </Select>
-        <Checkbox
-          name="isAgreed"
-          isChecked={formData.isAgreed}
-          onChange={handleChange}
-        >
-          規約に同意する
-        </Checkbox>
+        <Checkbox {...register('isAgreed')}>規約に同意する</Checkbox>
         <ButtonGroup my={3} w="xs">
-          <Button w="30%" colorScheme="orange" variant="solid" type="reset">
+          <Button
+            w="30%"
+            colorScheme="orange"
+            variant="solid"
+            onClick={onReset}
+          >
             リセット
           </Button>
           <Button w="70%" colorScheme="blue" variant="solid" type="submit">
